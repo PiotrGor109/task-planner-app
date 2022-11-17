05b6a2e6612c5b2728a97805d2791fc061f46a74
PiotrGor109 <piotrekgo@o2.pl>
2022-11-17 10:00:35 +0100

    Initialize application first time

---
+++ src/test/java/com/taskplanner/taskplannerapp/TaskPlannerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskplanner.taskplannerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

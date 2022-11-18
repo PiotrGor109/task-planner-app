2fbd3aacbebf85998ddacb2c2d66d093b5b2a15c
PiotrGor109 <piotrekgo@o2.pl>
2022-11-18 12:59:05 +0100

    Add POST, DELETE method mapping

--- src/main/java/com/taskplanner/taskplannerapp/controller/TaskGroupController.java
+++ src/main/java/com/taskplanner/taskplannerapp/controller/TaskGroupController.java
@@ -3,12 +3,13 @@ package com.taskplanner.taskplannerapp.controller;
 
 import com.taskplanner.taskplannerapp.model.TaskGroup;
 import com.taskplanner.taskplannerapp.model.TaskGroupRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +19,7 @@ public class TaskGroupController {
 
     private final TaskGroupRepository repository;
     private final ApplicationEventPublisher eventPublisher;
+    private static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
 
     TaskGroupController(final ApplicationEventPublisher eventPublisher, final TaskGroupRepository repository)
     {
@@ -26,12 +28,31 @@ public class TaskGroupController {
     }
 
 
-
     @GetMapping
     ResponseEntity<List<TaskGroup>> readAllTaskGroups(Pageable page)
     {
+        logger.info("TASK-GROUP-CONTROLLER - GET METHOD");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
+
+    @PostMapping
+    ResponseEntity<TaskGroup> createTaskGroup(@RequestBody TaskGroup newTaskGroup)
+    {
+        logger.info("TASK-GROUP-CONTROLLER - POST METHOD");
+        TaskGroup newTaskGroupUnit = repository.save(newTaskGroup);
+        return new ResponseEntity<>(newTaskGroup, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    ResponseEntity<Void> deleteTaskGroup(@PathVariable int id) throws Exception {
+        logger.info("TASK-GROUP-CONTROLLER - DELETE METHOD - DELETE TASKGROUP WITH ID: "+id);
+        repository.delete(repository.findById(id).orElse(null));
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
 
 
 }
--- src/main/java/com/taskplanner/taskplannerapp/controller/TasksController.java
+++ src/main/java/com/taskplanner/taskplannerapp/controller/TasksController.java
@@ -1,13 +1,14 @@
 package com.taskplanner.taskplannerapp.controller;
-
-
 import com.taskplanner.taskplannerapp.model.Tasks;
 import com.taskplanner.taskplannerapp.model.TasksRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,18 +16,41 @@ import java.util.List;
 @RequestMapping("/tasks")
 public class TasksController {
 
-
+    private final ApplicationEventPublisher eventPublisher;
     private final TasksRepository repository;
+    private static final Logger logger = LoggerFactory.getLogger(TasksController.class);
 
-    public TasksController(final TasksRepository repository) {
+    public TasksController(final ApplicationEventPublisher eventPublisher, final TasksRepository repository) {
         this.repository = repository;
+        this.eventPublisher = eventPublisher;
     }
 
 
     @GetMapping
     ResponseEntity<List<Tasks>> readAllTasks(Pageable page)
     {
+        logger.info("TASKS CONTROLLER - GET METHOD");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
+
+    @PostMapping
+    ResponseEntity<Tasks> createTask(@RequestBody Tasks newTasks)
+    {
+        logger.info("TASK CONTROLLER - POST METHOD");
+        Tasks newTask = repository.save(newTasks);
+        return new ResponseEntity<>(newTask, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    ResponseEntity<Void> deleteTask(@PathVariable int id) throws Exception {
+        logger.info("TASK CONTROLLER - DELETE METHOD - DELETE TASK WITH ID: "+id);
+        repository.delete(repository.findById(id).orElse(null));
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
 
 }
